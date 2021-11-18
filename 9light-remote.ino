a58963fa2c49d85c5aca00484e7ce1ef34b39c60
Niklas Bettgen <niklas@bettgen.de>
2021-11-18 12:28:08 +0100

    Proper debouncing timing applied

--- 9light-remote.ino
+++ 9light-remote.ino
@@ -26,10 +26,10 @@ void setup()
     SerialUSB.begin(115200);
 
     // Register remote buttons
-    Remote.registerButton(NineLightRemote::state::CALL,     10,     500);
-    Remote.registerButton(NineLightRemote::state::VIDEO,    11,     500);
-    Remote.registerButton(NineLightRemote::state::COFFEE,   12,     500);
-    Remote.registerButton(NineLightRemote::state::NONE,     13,     500);
+    Remote.registerButton(NineLightRemote::state::CALL,     10,     40);
+    Remote.registerButton(NineLightRemote::state::VIDEO,    11,     40);
+    Remote.registerButton(NineLightRemote::state::COFFEE,   12,     40);
+    Remote.registerButton(NineLightRemote::state::NONE,     13,     40);
 
     // Setup automatic status requests after being idle (keep remote registration status)
     Remote.setupIdleRequest(2UL * 60 * 60 * 1000);
