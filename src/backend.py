8eece1491f6ba61aa66d35cfebfaf30ead4ac4ea
Niklas Bettgen <niklas@bettgen.de>
2022-08-21 13:18:09 +0200

    Better instance handling

--- src/backend.py
+++ src/backend.py
@@ -17,39 +17,37 @@ from constants import (
 
 class Backend:
     """Container for the backend flask application."""
-    nl_instance: NineLight
 
     def __init__(self, nl_instance: NineLight) -> None:
-        Backend.nl_instance = nl_instance
+        self.nl_instance: NineLight = nl_instance
         self.app: Flask = Flask(__name__)
         self.app.secret_key = uuid4().hex
 
         @self.app.route("/status/get", methods=["GET"])
         def _status_get():
-            return Backend.status()
+            return self.status()
 
         @self.app.route("/status/set", methods=["GET"])
         def _status_set():
-            return Backend.status()
+            return self.status()
 
-    @staticmethod
-    def status() -> Response:
+    def status(self) -> Response:
         """Gets the bare status of the system and - if provided - updates a
         remote registration."""
 
         if "remote" in request.args:
-            Backend.nl_instance.add_remote(NineLightRemote(
+            self.nl_instance.add_remote(NineLightRemote(
                 str(request.remote_addr),
                 PORT_REMOTE
             ))
 
         new_state: Optional[str] = request.args.get("status")
         if new_state:
-            Backend.nl_instance.set_state(new_state)
+            self.nl_instance.set_state(new_state)
 
         return jsonify({
-            "state": Backend.nl_instance.get_state(),
-            "remotes": [r.ip_addr for r in Backend.nl_instance.remotes]
+            "state": self.nl_instance.get_state(),
+            "remotes": [r.ip_addr for r in self.nl_instance.remotes]
         })
 
     def run(self, port, host: str = "0.0.0.0") -> None:
--- src/frontend.py
+++ src/frontend.py
@@ -20,11 +20,10 @@ from constants import (
 
 class Frontend:
     """Container for the frontend flask application."""
-    nl_instance: NineLight
 
     def __init__(self, nl_instance: NineLight, template_folder: str,
                  static_folder: str) -> None:
-        Frontend.nl_instance = nl_instance
+        self.nl_instance: NineLight = nl_instance
         self.app: Flask = Flask(
             __name__,
             template_folder=abspath(template_folder),
@@ -34,16 +33,15 @@ class Frontend:
 
         @self.app.route("/", methods=["GET", "POST"])
         def _route_index():
-            return Frontend.index()
+            return self.index()
 
-    @staticmethod
-    def index() -> str:
+    def index(self) -> str:
         """Returns the index page of the web application."""
 
         # Trigger actions if requested
         if request.method == "POST":
             if "set-state" in request.form:
-                Frontend.nl_instance.set_state(request.form["new-state"])
+                self.nl_instance.set_state(request.form["new-state"])
 
             if "add-remote" in request.form:
                 result = match(r"^((?:\d{1,3}\.){3}\d{1,3})(?:\:(\d+))?$",
@@ -52,25 +50,25 @@ class Frontend:
                     groups = result.groups()
                     ip_addr: str = groups[0]
                     port: Union[str, int] = groups[1] or PORT_REMOTE
-                    Frontend.nl_instance.add_remote(
+                    self.nl_instance.add_remote(
                         NineLightRemote(ip_addr, int(port))
                     )
 
             if "del-remote" in request.form:
-                Frontend.nl_instance.delete_remote(
+                self.nl_instance.delete_remote(
                     NineLightRemote(request.form["del-remote"], 0)
                 )
 
             if "bell-button" in request.form:
                 if request.form["bell-button"] == "1":
-                    Frontend.nl_instance.on_bell_button()
+                    self.nl_instance.on_bell_button()
 
         return render_template(
             "frontend.html",
             main_title=MAIN_TITLE,
             client_ip=request.remote_addr,
-            current_state=Frontend.nl_instance.get_state(),
-            remotes=enumerate(Frontend.nl_instance.remotes)
+            current_state=self.nl_instance.get_state(),
+            remotes=enumerate(self.nl_instance.remotes)
         )
 
     def run(self, port, host: str = "0.0.0.0") -> None:
